19150b5f7e018d8af5d6c2edba71280d9fbfa99f
Emily Miles <emiles4@gmail.com>
2018-10-19 04:36:16 +0000

    automatically backed up by learn

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
 collection = [1, 2, 3, 4]
 def my_each(collection)
-  i = 0
-  while i < array.length
-    yield(array[i])
-    i = i + 1
-  end
-  puts i
+i = 0
+while i < array.length
+  yield(array[i])
+i = i + 1
+end
+array
 end
